08c500a1380833441cc077ebadcdabd56211a783
Keval <kevalmehta885@gmail.com>
2024-10-08 15:55:32 +0530

    Providing the Applayout & other route as child element

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,31 @@ import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
+import AppLayout from "./UI/AppLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "order/new",
+        element: <CreateOrder />,
+      },
+      { path: "/order/:orderID", element: <Order /> },
+    ],
   },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "order/new",
-    element: <CreateOrder />,
-  },
-  { path: "/order/:orderID", element: <Order /> },
 ]);
 
 function App() {
---
+++ src/UI/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast Pizza Co.</Link>
+      <p>Keval Mehta</p>
+    </header>
+  );
+}
+
+export default Header;
