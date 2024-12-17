169ef003aaed053e688b1e4717b940ae964cb77a
Keval <kevalmehta885@gmail.com>
2024-12-17 16:36:27 +0530

    Solved Bug and updated Code

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./UI/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Order, { loader as orderLoader } from "./features/order/Order";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
 import Cart from "./features/cart/Cart";
 import CreateOrder, {
   action as createOrderAction,
@@ -36,10 +37,11 @@ const router = createBrowserRouter([
         action: createOrderAction,
       },
       {
-        path: "/order/:orderID",
+        path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
